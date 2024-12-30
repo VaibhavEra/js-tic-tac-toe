dbad7bfae2d27163b1848a2245c18aa4c75ec74d
Vaibhav Saini <Vaibhavsaini.msrit@gmail.com>
2024-12-31 03:58:49 +0530

    update app.js to include initial setup

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-console.log("Works");
+const gameBoard = (function () {
+  const grid = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+
+  const fetchGrid = () => grid;
+  const updateGrid = (choice, pos1, pos2) => (grid[pos1][pos2] = choice);
+
+  return { fetchGrid, updateGrid, grid };
+})();
+
+const gameController = (function () {
+  //input player choice
+  let symbolX = true;
+  const choice = (position1, position2) => {
+    if (symbolX) {
+      gameBoard.updateGrid("X", position1, position2);
+      symbolX = false;
+    } else {
+      gameBoard.updateGrid("O", position1, position2);
+      symbolX = true;
+    }
+    checkWinCondition();
+  };
+
+  function checkWinCondition() {}
+
+  return { choice };
+})();
+
+function createPlayer(name) {
+  const displayName = "@" + name;
+  const { choice } = gameController;
+  return { displayName, choice };
+}
+
+const bill = createPlayer("bill");
+const stan = createPlayer("stan");
